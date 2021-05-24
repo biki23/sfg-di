1a7c000b5cb975f8b22274cce619205a231bd265
Bikram Kashyap <kashyap.bikram@gmail.com>
2021-05-24 00:01:19 -0400

    Moved pet service to java based configuration

--- src/main/java/guru/springframework/pets/CatPetService.java
+++ src/main/java/guru/springframework/pets/CatPetService.java
@@ -1,11 +1,13 @@
 package guru.springframework.pets;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
-@Service("cat")
+
 public class CatPetService implements PetService {
     @Override
     public String getPetType() {
